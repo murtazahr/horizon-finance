c9f56266232d454c9d068643742b2873216e8d79
Murtaza Rangwala <murtazahatimr@icloud.com>
2024-05-04 14:45:04 +0530

    Update missing config and css files.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className='home'>
+            <div className='home-content'>
+            </div>
+        </section>
     )
 }
 
